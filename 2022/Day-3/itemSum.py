f151439c1e4c3e43b46a096c7478bbcf9c2fb4cf
Matt Scalzo <mattcrunch@gmail.com>
2022-12-08 00:56:53 -0500

    Solve Day-3 Challenges

---
+++ 2022/Day-3/itemSum.py
@@ -0,0 +1,58 @@
+input = open('input.txt', 'r')
+lines = input.readlines()
+
+sum = 0
+badgeSum = 0
+elfNum = 0
+badgeSet1 = set()
+badgeSet2 = set()
+badgeSet3 = set()
+
+for line in lines:
+    rucksack = line.strip()
+
+    compartment1 = rucksack[0:len(rucksack)//2]
+    compartment2 = rucksack[len(rucksack)//2:]
+
+    for item in compartment1:
+        if item in compartment2:
+            asciiNum = ord(item)
+
+            if asciiNum < 97:
+                sum = sum + asciiNum - 38
+
+            else:
+                sum = sum + asciiNum - 96
+
+            break
+
+    if elfNum % 3 == 0:
+        badgeSet1 = set()
+        badgeSet2 = set()
+        badgeSet3 = set()
+        for item in rucksack:
+            badgeSet1.add(item)
+
+    if elfNum % 3 == 1:
+        for item in rucksack:
+            badgeSet2.add(item)
+
+    if elfNum % 3 == 2:
+        for item in rucksack:
+            badgeSet3.add(item)
+
+        badgeSet = badgeSet1.intersection(badgeSet2, badgeSet3)
+        badge = ord(badgeSet.pop())
+
+        if badge < 97:
+            badgeSum = badgeSum + badge - 38
+
+        else:
+            badgeSum = badgeSum + badge - 96
+            
+    elfNum = elfNum + 1
+
+input.close()
+
+print("Duplicate Item Sum: " + str(sum))
+print("Badges Sum: " + str(badgeSum))
